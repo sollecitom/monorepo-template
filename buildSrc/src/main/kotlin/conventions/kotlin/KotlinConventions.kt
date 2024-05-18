18fa32bfbfac35b70cb72838e138c671c4ed69bc
michele <michele.sollecito@gmail.com>
2024-05-19 00:14:19 +0100

    WIP

--- buildSrc/src/main/kotlin/conventions/kotlin/KotlinConventions.kt
+++ buildSrc/src/main/kotlin/conventions/kotlin/KotlinConventions.kt
@@ -1,19 +1,25 @@
 package conventions.kotlin
 
-import KotlinConfiguration
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+private val optIns = listOf("kotlin.Experimental", "kotlinx.coroutines.ExperimentalCoroutinesApi")
+private val optInCompilerArguments = optIns.map { "-opt-in=$it" }
+private val compilerArgs = optInCompilerArguments + listOf("-Xcontext-receivers")
+private val targetJvmVersion = "21"
+
 class KotlinConventions : Plugin<Project> {
 
     override fun apply(project: Project) {
+
         project.tasks.withType<KotlinCompile>().configureEach {
+
             kotlinOptions.apply {
-                jvmTarget = KotlinConfiguration.TargetJvm.version
-                javaParameters = KotlinConfiguration.Compiler.generateJavaParameters
-                freeCompilerArgs = KotlinConfiguration.Compiler.arguments
+                jvmTarget = targetJvmVersion
+                javaParameters = true
+                freeCompilerArgs = compilerArgs
             }
         }
     }
