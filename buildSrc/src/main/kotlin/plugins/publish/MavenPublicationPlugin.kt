9d9ab1c5565829faa2d8f3a4ff4ba9b62280d35f
michele <michele.sollecito@gmail.com>
2024-05-19 00:52:02 +0100

    WIP

--- buildSrc/src/main/kotlin/plugins/publish/MavenPublicationPlugin.kt
+++ buildSrc/src/main/kotlin/plugins/publish/MavenPublicationPlugin.kt
@@ -1,14 +1,12 @@
 package plugins.publish
 
 import RepositoryConfiguration
-import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.api.publish.PublicationContainer
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
+import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
 
@@ -17,35 +15,21 @@ class MavenPublicationPlugin : Plugin<Project> {
 
     override fun apply(project: Project) {
 
-        project.pluginManager.apply(MavenPublishPlugin::class.java)
+        project.pluginManager.apply(MavenPublishPlugin::class)
 
-        project.extensions.configure(PublishingExtension::class, object : Action<PublishingExtension> {
-
-            override fun execute(publishing: PublishingExtension) {
-
-                publishing.repositories(object : Action<RepositoryHandler> {
-                    override fun execute(repositories: RepositoryHandler) {
-                        RepositoryConfiguration.Publications.apply(repositories, project)
-                    }
-                })
-
-                publishing.publications(object : Action<PublicationContainer> {
-
-                    override fun execute(publications: PublicationContainer) {
-
-                        publications.create("${project.name}-maven", MavenPublication::class.java, object : Action<MavenPublication> {
-
-                            override fun execute(publication: MavenPublication) {
-                                publication.groupId = project.rootProject.group.toString()
-                                publication.artifactId = project.name
-                                publication.version = project.rootProject.version.toString()
-                                publication.from(project.components["java"])
-                                println("Created publication ${publication.groupId}:${publication.artifactId}:${publication.version}")
-                            }
-                        })
-                    }
-                })
+        project.extensions.configure(PublishingExtension::class) {
+            repositories {
+                RepositoryConfiguration.Publications.apply(this, project)
             }
-        })
+            publications {
+                create("${project.name}-maven", MavenPublication::class.java) {
+                    groupId = project.rootProject.group.toString()
+                    artifactId = project.name
+                    version = project.rootProject.version.toString()
+                    from(project.components["java"])
+                    println("Created publication ${groupId}:${artifactId}:${version}")
+                }
+            }
+        }
     }
 }
