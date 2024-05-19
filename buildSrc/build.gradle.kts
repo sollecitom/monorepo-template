f26f2a6ebf0c486640c655c316722e65008c477e
michele <michele.sollecito@gmail.com>
2024-05-19 13:40:39 +0100

    WIP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -19,3 +19,4 @@ dependencies {
 // TODO read and use https://docs.gradle.org/current/userguide/implementing_gradle_plugins_binary.html
 // TODO read and use https://docs.gradle.org/current/userguide/custom_gradle_types.html#service_injection
 // TODO read https://dev.to/autonomousapps/gradle-plugins-and-extensions-a-primer-for-the-bemused-51lp
+// TODO read https://www.baeldung.com/gradle-create-plugin
