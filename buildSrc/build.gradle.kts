d1031d8122d00b2814f8df9bc83ff99614831f16
michele <michele.sollecito@gmail.com>
2024-05-19 13:33:48 +0100

    WIP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,3 +17,4 @@ dependencies {
 
 // TODO read and use https://docs.gradle.org/current/userguide/custom_plugins.html#sec:build_script_plugins for different types of plugins
 // TODO read and use https://docs.gradle.org/current/userguide/implementing_gradle_plugins_binary.html
+// TODO read and use https://docs.gradle.org/current/userguide/custom_gradle_types.html#service_injection
