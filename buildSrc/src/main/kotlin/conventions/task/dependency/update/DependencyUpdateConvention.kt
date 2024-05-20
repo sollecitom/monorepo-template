9001dd112b8006d7949bcc51ed0ff694bd16350a
Michele Sollecito <michele.sollecito@gmail.com>
2024-05-20 11:40:48 +0100

    WIP

--- buildSrc/src/main/kotlin/conventions/task/dependency/update/DependencyUpdateConvention.kt
+++ buildSrc/src/main/kotlin/conventions/task/dependency/update/DependencyUpdateConvention.kt
@@ -3,24 +3,30 @@ package conventions.task.dependency.update
 import com.github.benmanes.gradle.versions.VersionsPlugin
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import com.github.benmanes.gradle.versions.updates.resolutionstrategy.ComponentSelectionWithCurrent
-import nl.littlerobots.vcu.plugin.VersionCatalogUpdateExtension
-import nl.littlerobots.vcu.plugin.VersionCatalogUpdatePlugin
+import nl.littlerobots.vcu.plugin.*
+import org.gradle.api.Action
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.file.RegularFileProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Nested
+import org.gradle.api.tasks.Optional
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.withType
 
 abstract class DependencyUpdateConvention : Plugin<Project> {
 
-    override fun apply(project: Project) = with(project) { // TODO introduce an extension that allows to override these settings
+    override fun apply(project: Project) = with(project) {
 
         pluginManager.apply(VersionCatalogUpdatePlugin::class)
         pluginManager.apply(VersionsPlugin::class)
+        val extension = project.extensions.create<Extension>("versionCatalog")
 
         afterEvaluate {
-
-            tasks.withType<DependencyUpdatesTask> {
+            tasks.withType<DependencyUpdatesTask> { // TODO make these configurable through the extension
                 checkConstraints = true
                 checkBuildEnvironmentConstraints = false
                 checkForGradleUpdate = true
@@ -34,14 +40,58 @@ abstract class DependencyUpdateConvention : Plugin<Project> {
             }
 
             extensions.configure<VersionCatalogUpdateExtension> {
-                sortByKey.set(false)
+                sortByKey.set(extension.sortByKey.getOrElse(defaultSortByKey))
+                extension.catalogFile.takeIf { it.isPresent }?.let {
+                    catalogFile.set(it.get())
+                }
                 keep {
-                    keepUnusedVersions.set(true)
-                    keepUnusedLibraries.set(true)
-                    keepUnusedPlugins.set(true)
+                    keepUnusedVersions.set(extension.keep.keepUnusedVersions.getOrElse(defaultKeepUnusedVersions))
+                    keepUnusedLibraries.set(extension.keep.keepUnusedLibraries.getOrElse(defaultKeepUnusedLibraries))
+                    keepUnusedPlugins.set(extension.keep.keepUnusedPlugins.getOrElse(defaultKeepUnusedPlugins))
+                    versions.set(extension.keep.versions.getOrElse(emptySet()))
+                    groups.set(extension.keep.groups.getOrElse(emptySet()))
+                    libraries.set(extension.keep.libraries.getOrElse(emptySet()))
+                    plugins.set(extension.keep.plugins.getOrElse(emptySet()))
+                }
+                pin {
+                    versions.set(extension.pins.versions.getOrElse(emptySet()))
+                    groups.set(extension.pins.groups.getOrElse(emptySet()))
+                    libraries.set(extension.pins.libraries.getOrElse(emptySet()))
+                    plugins.set(extension.pins.plugins.getOrElse(emptySet()))
                 }
             }
         }
+    }
+
+    private companion object {
+        const val defaultSortByKey = false
+        const val defaultKeepUnusedVersions = true
+        const val defaultKeepUnusedLibraries = true
+        const val defaultKeepUnusedPlugins = true
+    }
+
+    abstract class Extension {
+
+        @get:Optional
+        abstract val sortByKey: Property<Boolean>
+
+        @get:Optional
+        abstract val catalogFile: RegularFileProperty
+
+        @get:Nested
+        abstract val keep: KeepConfiguration
+
+        @get:Nested
+        abstract val pins: PinConfiguration
+
+        @get:Nested
+        abstract val versionCatalogs: NamedDomainObjectContainer<VersionCatalogConfig>
+
+        fun keep(action: Action<KeepConfiguration>) = action.execute(keep)
+
+        fun pins(action: Action<PinConfiguration>) = action.execute(pins)
+
+        fun versionCatalogs(action: Action<NamedDomainObjectContainer<VersionCatalogConfig>>) = action.execute(versionCatalogs)
     }
 
     private val ComponentSelectionWithCurrent.currentSemanticVersion: DependencyVersion get() = DependencyVersion(currentVersion)
